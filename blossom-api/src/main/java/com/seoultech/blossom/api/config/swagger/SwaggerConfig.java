7b51ce37a24783c4d8045a15a38354f5db81753a
HyeokJoon Kong <orijoon98@gmail.com>
2023-05-03 00:15:41 +0900

    #1 🔧 chore: swagger 설정 수정

--- blossom-api/src/main/java/com/seoultech/blossom/api/config/swagger/SwaggerConfig.java
+++ blossom-api/src/main/java/com/seoultech/blossom/api/config/swagger/SwaggerConfig.java
@@ -1,5 +1,7 @@
 package com.seoultech.blossom.api.config.swagger;
 
+import java.util.Arrays;
+
 import org.springdoc.core.SpringDocUtils;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -9,19 +11,28 @@ import com.seoultech.blossom.api.config.resolver.UserId;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
 
 @Configuration
 public class SwaggerConfig {
 
 	@Bean
-	public OpenAPI customOpenAPI() {
-		return new OpenAPI().components(new Components().addSecuritySchemes("Authorization", new SecurityScheme()
-				.type(SecurityScheme.Type.HTTP).scheme("bearer").bearerFormat("JWT")
-				.in(SecurityScheme.In.HEADER).name("Authorization")))
-			.info(new Info()
-				.title("Blossom API Server")
-				.description("Blossom API Docs"));
+	public OpenAPI openAPI() {
+		Info info = new Info()
+			.title("Blossom API Server")
+			.description("Blossom API Docs")
+			.version("1.0.0");
+
+		SecurityScheme securityScheme = new SecurityScheme()
+			.type(SecurityScheme.Type.HTTP).scheme("bearer").bearerFormat("JWT")
+			.in(SecurityScheme.In.HEADER).name("Authorization");
+		SecurityRequirement securityRequirement = new SecurityRequirement().addList("bearerAuth");
+
+		return new OpenAPI()
+			.components(new Components().addSecuritySchemes("bearerAuth", securityScheme))
+			.security(Arrays.asList(securityRequirement))
+			.info(info);
 	}
 
 	static {
