7e65c2d7cfc21e01a5f7845329619e354cfcc6e6
HyeokJoon Kong <orijoon98@gmail.com>
2023-05-14 14:54:23 +0900

    #3 🔧 chore: CI/CD 구축

---
+++ blossom-api/src/main/java/com/seoultech/blossom/api/controller/health/HealthController.java
@@ -0,0 +1,13 @@
+package com.seoultech.blossom.api.controller.health;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController {
+
+	@GetMapping("/health")
+	public String checkHealth() {
+		return "healthy";
+	}
+}
