8d1f768418bc9b8e2c49cd6a5356e9d285f6a302
HyeokJoon Kong <orijoon98@gmail.com>
2023-05-14 15:18:12 +0900

    #6 🔧 chore: 스키마 생성 sql 추가

--- blossom-api/src/main/resources/sql/schema.sql
+++ blossom-api/src/main/resources/sql/schema.sql
@@ -1,10 +1,110 @@
-DROP TABLE IF EXISTS user;
+DROP TABLE IF EXISTS `USER`;
+DROP TABLE IF EXISTS `FLOWER`;
+DROP TABLE IF EXISTS `FLOWER_IMAGE`;
+DROP TABLE IF EXISTS `FLOWER_TAG`;
+DROP TABLE IF EXISTS `FLOWER_LANGUAGE`;
+DROP TABLE IF EXISTS `FLOWER_TIME`;
+DROP TABLE IF EXISTS `FLOWER_CARE`;
+DROP TABLE IF EXISTS `BETTER_TOGETHER`;
+DROP TABLE IF EXISTS `FLOWER_CONTENTS`;
+DROP TABLE IF EXISTS `FLOWER_LIKES`;
 
-CREATE TABLE user
+CREATE TABLE `USER`
 (
-    id          bigint auto_increment primary key,
-    social_id   varchar(300) not null,
-    social_type varchar(30)  not null,
-    created_at  datetime(6) not null,
-    modified_at datetime(6) not null
+    `USER_ID`           bigint auto_increment primary key,
+    `SOCIAL_ID`         varchar(300) NOT NULL,
+    `SOCIAL_TYPE`       varchar(30)  NOT NULL,
+    `NICKNAME`          varchar(30)  NOT NULL,
+    `PROFILE_IMAGE_URL` varchar(300) NULL,
+    `CREATED_AT`        datetime     NOT NULL,
+    `MODIFIED_AT`       datetime     NOT NULL
 );
+
+CREATE TABLE `FLOWER`
+(
+    `FLOWER_ID`              bigint auto_increment primary key,
+    `KOREAN_NAME`            varchar(30)  NOT NULL,
+    `ENGLISH_NAME`           varchar(100) NOT NULL,
+    `CATEGORY`               varchar(30)  NOT NULL,
+    `FAMILY_NAME`            varchar(30)  NOT NULL,
+    `ADDITIONAL_INFORMATION` varchar(100) NOT NULL,
+    `FIT_NAME`               varchar(30)  NOT NULL,
+    `FIT_INFORMATION`        varchar(100) NOT NULL,
+    `CREATED_AT`             datetime     NOT NULL,
+    `MODIFIED_AT`            datetime     NOT NULL
+);
+
+CREATE TABLE `FLOWER_IMAGE`
+(
+    `FLOWER_IMAGE_ID`  bigint auto_increment primary key,
+    `FLOWER_ID`        bigint       NOT NULL,
+    `FLOWER_IMAGE_URL` varchar(300) NOT NULL,
+    `ORDER`            int          NOT NULL,
+    `CREATED_AT`       datetime     NOT NULL,
+    `MODIFIED_AT`      datetime     NOT NULL
+);
+
+CREATE TABLE `FLOWER_TAG`
+(
+    `FLOWER_TAG_ID` bigint auto_increment primary key,
+    `FLOWER_ID`     bigint      NOT NULL,
+    `NAME`          varchar(30) NOT NULL,
+    `CREATED_AT`    datetime    NOT NULL,
+    `MODIFIED_AT`   datetime    NOT NULL
+);
+
+CREATE TABLE `FLOWER_LANGUAGE`
+(
+    `FLOWER_LANGUAGE_ID` bigint auto_increment primary key,
+    `FLOWER_ID`          bigint      NOT NULL,
+    `NAME`               varchar(30) NOT NULL,
+    `CREATED_AT`         datetime    NOT NULL,
+    `MODIFIED_AT`        datetime    NOT NULL
+);
+
+CREATE TABLE `FLOWER_TIME`
+(
+    `FLOWER_TIME_ID` bigint auto_increment primary key,
+    `FLOWER_ID`      bigint   NOT NULL,
+    `MONTH`          int      NOT NULL,
+    `CREATED_AT`     datetime NOT NULL,
+    `MODIFIED_AT`    datetime NOT NULL
+);
+
+CREATE TABLE `FLOWER_CARE`
+(
+    `FLOWER_CARE_ID` bigint auto_increment primary key,
+    `FLOWER_ID`      bigint       NOT NULL,
+    `INFORMATION`    varchar(100) NOT NULL,
+    `ORDER`          int          NOT NULL,
+    `CREATED_AT`     datetime     NOT NULL,
+    `MODIFIED_AT`    datetime     NOT NULL
+);
+
+CREATE TABLE `BETTER_TOGETHER`
+(
+    `BETTER_TOGETHER_ID`        bigint auto_increment primary key,
+    `FLOWER_ID`                 bigint   NOT NULL,
+    `BETTER_TOGETHER_FLOWER_ID` bigint   NOT NULL,
+    `CREATED_AT`                datetime NOT NULL,
+    `MODIFIED_AT`               datetime NOT NULL
+);
+
+CREATE TABLE `FLOWER_CONTENTS`
+(
+    `FLOWER_CONTENTS_ID` bigint auto_increment primary key,
+    `FLOWER_ID`          bigint   NOT NULL,
+    `CONTENTS_ID`        bigint   NOT NULL,
+    `CREATED_AT`         datetime NOT NULL,
+    `MODIFIED_AT`        datetime NOT NULL
+);
+
+CREATE TABLE `FLOWER_LIKES`
+(
+    `FLOWER_LIKES_ID` bigint auto_increment primary key,
+    `USER_ID`         bigint   NOT NULL,
+    `FLOWER_ID`       bigint   NOT NULL,
+    `CREATED_AT`      datetime NOT NULL,
+    `MODIFIED_AT`     datetime NOT NULL
+);
+
