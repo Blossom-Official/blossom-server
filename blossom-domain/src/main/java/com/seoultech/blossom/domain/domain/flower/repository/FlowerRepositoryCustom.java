0ac380ea16b623c08820f30fa2797c9a0df661f4
HyeokJoon Kong <orijoon98@gmail.com>
2023-05-17 19:59:10 +0900

    #11 ✨ feat: 카테고리별 꽃 조회 API 구현

--- blossom-domain/src/main/java/com/seoultech/blossom/domain/domain/flower/repository/FlowerRepositoryCustom.java
+++ blossom-domain/src/main/java/com/seoultech/blossom/domain/domain/flower/repository/FlowerRepositoryCustom.java
@@ -1,8 +1,13 @@
 package com.seoultech.blossom.domain.domain.flower.repository;
 
+import java.util.List;
+
 import com.seoultech.blossom.domain.domain.flower.Flower;
+import com.seoultech.blossom.domain.domain.flower.FlowerCategory;
 
 public interface FlowerRepositoryCustom {
 
 	Flower findFlowerById(Long id);
+
+	List<Flower> findFlowersByCategory(FlowerCategory category);
 }
