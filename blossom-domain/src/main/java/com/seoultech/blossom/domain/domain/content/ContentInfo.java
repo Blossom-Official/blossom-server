18cabb098bcf6fc235e551ef23faaf0f40971385
HyeokJoon Kong <orijoon98@gmail.com>
2023-05-29 17:04:32 +0900

    #22 ✨ feat: 컨텐츠 관련 도메인 개발

--- blossom-domain/src/main/java/com/seoultech/blossom/domain/domain/content/ContentInfo.java
+++ blossom-domain/src/main/java/com/seoultech/blossom/domain/domain/content/ContentInfo.java
@@ -1,4 +1,4 @@
-package com.seoultech.blossom.domain.domain.flower;
+package com.seoultech.blossom.domain.domain.content;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -8,11 +8,9 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
-import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
 import com.seoultech.blossom.domain.domain.common.BaseEntity;
-import com.seoultech.blossom.domain.domain.content.Content;
 
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
@@ -20,24 +18,32 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-@Table(name = "FLOWER_CONTENT")
+@Table(name = "CONTENT_INFO")
 @Getter
 @Entity
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
 @Builder(access = AccessLevel.PRIVATE)
-public class FlowerContent extends BaseEntity {
+public class ContentInfo extends BaseEntity {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	@Column(name = "FLOWER_CONTENT_ID")
+	@Column(name = "CONTENT_INFO_ID")
 	private Long id;
 
 	@ManyToOne(fetch = FetchType.LAZY)
-	@JoinColumn(name = "FLOWER_ID", nullable = false)
-	private Flower flower;
-
-	@OneToOne(fetch = FetchType.LAZY)
 	@JoinColumn(name = "CONTENT_ID", nullable = false)
 	private Content content;
+
+	@Column(name = "CONTENT_IMAGE_URL", nullable = false, length = 300)
+	private String contentImageUrl;
+
+	@Column(name = "ORDER", nullable = false)
+	private Integer order;
+
+	@Column(name = "LINK_YN", nullable = false)
+	private Boolean linkYn;
+
+	@Column(name = "LINK_URL", nullable = true, length = 300)
+	private String linkUrl;
 }
