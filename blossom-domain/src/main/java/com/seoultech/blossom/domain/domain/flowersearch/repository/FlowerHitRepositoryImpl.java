e7dac4db301b4e49ca022902338aed19a6fd06fe
HyeokJoon Kong <orijoon98@gmail.com>
2023-06-15 17:12:19 +0900

    #25 ⚡️ fix: 비관적 락 querydsl 에서 적용되도록 수정

--- blossom-domain/src/main/java/com/seoultech/blossom/domain/domain/flowersearch/repository/FlowerHitRepositoryImpl.java
+++ blossom-domain/src/main/java/com/seoultech/blossom/domain/domain/flowersearch/repository/FlowerHitRepositoryImpl.java
@@ -4,8 +4,6 @@ import static com.seoultech.blossom.domain.domain.flowersearch.QFlowerHit.*;
 
 import javax.persistence.LockModeType;
 
-import org.springframework.data.jpa.repository.Lock;
-
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import com.seoultech.blossom.domain.domain.flower.Flower;
 import com.seoultech.blossom.domain.domain.flowersearch.FlowerHit;
@@ -18,11 +16,11 @@ public class FlowerHitRepositoryImpl implements FlowerHitRepositoryCustom {
 	private final JPAQueryFactory queryFactory;
 
 	@Override
-	@Lock(LockModeType.PESSIMISTIC_WRITE)
 	public FlowerHit findFlowerHitByFlower(Flower flower) {
 		return queryFactory
 			.selectFrom(flowerHit)
 			.where(flowerHit.flower.eq(flower))
+			.setLockMode(LockModeType.PESSIMISTIC_WRITE)
 			.fetchOne();
 	}
 }
